3c6f7fc5cc1d3d4f135c49d6c058027a487a439b
ikenozhuo <34759932+ikenozhuo@users.noreply.github.com>
2025-01-14 12:20:24 +0800

    Create update.js

---
+++ update.js
@@ -0,0 +1,8 @@
+{
+    softwareName: "自动记账@大鹅本鹅",
+    version: "3.0",
+    releaseDate: "2025-01-14",
+    author: "@大鹅本鹅",
+    contact: "bigoose2077",
+    updateUrl: "https://www.icloud.com/shortcuts/99cf84f366fc4194b88ad39cc2cdccb8"
+}
